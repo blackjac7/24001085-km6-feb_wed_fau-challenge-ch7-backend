44dfa161a4028965a54b597d743a80ad86b24f0b
blackjac7 <febriansyah705@gmail.com>
2024-05-06 22:03:16 +0700

    add: adding environment staging & production

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World Binarians!");
 });
 
 app.listen(PORT, () => {
