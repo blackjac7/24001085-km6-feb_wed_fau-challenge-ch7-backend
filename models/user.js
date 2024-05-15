79f80923efef6fb339e13f863b420e9e9e5a61c0
blackjac7 <febriansyah705@gmail.com>
2024-05-15 22:05:22 +0700

    add: adding relation in model

--- models/user.js
+++ models/user.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+            User.hasMany(models.Message, {
+                foreignKey: "sender_id",
+            });
         }
     }
     User.init(
