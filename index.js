ccd16bcbf715d88dd883933bea89977fa6900992
FauzanR06 <fauzanm157@gmail.com>
2024-05-18 02:23:15 +0700

    fix socket typing

--- index.js
+++ index.js
@@ -5,29 +5,59 @@ const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const routes = require("./routers");
 const errorHandler = require("./middlewares/errorHandler");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use(
-    fileUpload({
-        useTempFiles: true,
-        tempFileDir: process.env.NODE_ENV == "development" ? "./tmp" : "/tmp", // if you're using GCP App Engine please don't comment this, because the ./tmp directory is read only and we need write too so we use /tmp
-    })
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: process.env.NODE_ENV == "development" ? "./tmp" : "/tmp", // if you're using GCP App Engine please don't comment this, because the ./tmp directory is read only and we need write too so we use /tmp
+  })
 );
+
+app.use(async function (req, res, next) {
+  req.io = io;
+  next();
+});
 
 app.use("/api", routes);
 
 app.use("*", (req, res) => {
-    res.status(404).json({ error: "Not Found" });
+  res.status(404).json({ error: "Not Found" });
 });
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
+const options = {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+};
+
+const server = http.createServer(app);
+
+const io = new Server(server, options);
+
+io.on("connection", (socket) => {
+  console.log(socket.id + " connected!");
+
+  socket.on("disconnect", (reason) => {
+    console.log(socket.id + " disconnected because " + reason);
+  });
+
+  socket.on("typing", (username) => {
+    console.log("typing trigger");
+    io.emit("ontyping", username);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
 });
--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth");
+const userRouter = require("./user");
 const messageRouter = require("./message");
 
 router.use("/auth", authRouter);
+router.use("/users", userRouter);
 router.use("/messages", messageRouter);
 
 module.exports = router;
