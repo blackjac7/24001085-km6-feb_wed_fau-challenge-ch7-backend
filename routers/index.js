a20fdfffed6fa2f7493f72ca9ea76743db9440c2
blackjac7 <febriansyah705@gmail.com>
2024-06-30 08:07:02 +0700

    test

--- routers/index.js
+++ routers/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const authRouter = require("./auth");
 const userRouter = require("./user");
 const messageRouter = require("./message");
+const airportController = require("../controllers/airport");
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/messages", messageRouter);
+router.get("/airports", airportController.getAirports);
 
 module.exports = router;
